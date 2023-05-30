ed16298fc284bab0f3190687c46bdeec8fed24d3
dimazsr <127843799+dimazsr@users.noreply.github.com>
2023-05-30 15:30:31 +0900

    MainView

--- NC1/NC1/MainView.swift
+++ NC1/NC1/MainView.swift
@@ -11,30 +11,33 @@ struct MainView: View {
 
     var body: some View {
         Spacer()
-            .frame(height: 35)
         
         VStack {
             Text("시비없는 12팀으로 알아보는")
                 .font(.system(size: 16))
+                .foregroundColor(Color("gray"))
+            
+            Text(" ")
+                .font(.system(size: 3))
             
             Text("나는 어떤 러너일까!?")
                 .fontWeight(.black)
+                .foregroundColor(Color("gray"))
                 .font(.system(size: 32))
-                .padding(.bottom, getWidth() * 0.2)
-                .padding(.top, getWidth() * 0.04)
+                .padding(.bottom, getWidth() * 0.15)
             
             Image("test")
                 .resizable()
-                .foregroundColor(.accentColor)
                 .frame(width: 200, height: 200)
-                .padding(.bottom, getWidth() * 0.04)
+                .padding(.bottom, getWidth() * 0.15)
             
             Spacer()
-                .frame(height: 14)
+                .frame(height: 5)
 
             Text("MC1에서 나는")
-                .font(.system(size: 20))
                 .fontWeight(.regular)
+                .font(.system(size: 20))
+            
             Text("어떤 성향의 러너였을지 알아보자!")
                 .font(.system(size: 20))
                 .fontWeight(.regular)
@@ -47,20 +50,18 @@ struct MainView: View {
             } label: {
                 Text("테스트 시작하기")
                     .frame(width: getWidth() * 0.7, height: getHeight() * 0.025)
-                    .foregroundColor(.white)
                     .fontWeight(.black)
+                    .foregroundColor(.white)
                     .font(.system(size: 20))
                     .padding(15)
-                    .background(Color.blue)
+                    .background(Color("blue"))
                     .cornerRadius(30)
-                    .padding(.top, getWidth() * 0.04)
-                    .padding(.top, getWidth() * 0.04)
+                    .padding(.top, getWidth() * 0.1)
             }
-            
-            Spacer()
-                .frame(height: 60)
-            
         }
+    
+        Spacer()
+        
     }
 }
 
